2c8ad3d2f5f9061f9c92258304bb458c509e3001
hakimled <hakimled30@gmail.com>
2023-10-21 19:24:38 +0100

    Two applications added to the project , a News application and a Bookstore application

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Bookstore(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    description =models.TextField()
+    price = models.DecimalField(decimal_places=2)
+    added_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    added_on = models.DateTimeField(default=timezone.now)
+    picture = models.ImageField(upload_to='pictures')
+    
+    def __str__(self):
+        return f'{self.author} added this book {self.title}'
+    
