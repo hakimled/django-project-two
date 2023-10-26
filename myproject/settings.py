825a4921064983ae63a79eedfd51d6cae21966e2
hakimled <hakimled30@gmail.com>
2023-10-26 11:21:43 +0100

    New modifications are added to thhe file

--- myproject/settings.py
+++ myproject/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -95,7 +95,7 @@ DATABASES = {
     }
 }
 
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
